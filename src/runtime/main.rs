17ff5722d39a5ea1993589c5bcaec6e93d938f15
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2026-03-30 16:18:47 -0500

    fixed mqqt device name

--- src/runtime/main.rs
+++ src/runtime/main.rs
@@ -175,7 +175,7 @@ fn main() -> anyhow::Result<()> {
 
     let mut mqtt = Box::new(Mqtt::new_mqtt(
         "mqttS://mqtt.jantaus.com:9443",
-        "device7_pub",
+        "device1_pub",
         &real_mqtt_user,
         &real_mqtt_pass,
     )?);
