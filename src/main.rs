2abd578d65d8b16457a28bf7019d6e9d0e94fe02
nathan-jw <nwignall@jantaus.com>
2026-07-14 13:58:09 -0500

    Better errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use core::time::Duration;
 use std::sync::mpsc;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use fsm::Fsm;
+use log::{error, info};
 
 use crate::logic::fsm::{
     led::{LEDContext, LEDHold},
@@ -10,6 +11,7 @@ use crate::logic::fsm::{
     motion::{MotionContext, MotionNotMoving},
     startup::{Initialization, StartupContext},
 };
+
 mod config;
 mod hardware;
 mod logic;
@@ -26,28 +28,30 @@ fn main() -> Result<()> {
     // startup
     let startup_events_tx = conductor_tx.clone();
     let (_startup_commands_tx, startup_commands_rx) = mpsc::channel();
-    let mut startup_result = Fsm::new_sync(
+
+    let startup_result = Fsm::new_sync(
         Box::new(Initialization),
         StartupContext::new(),
         startup_events_tx,
         startup_commands_rx,
-    )?;
+    )
+    .context("Failed to start Startup FSM")?;
 
-    // get state
     let StartupContext {
         switchboard,
-        sysloop,
+        sysloop: _,
         nvs,
         peripherals,
     } = startup_result;
 
-    let _switchboard = switchboard.unwrap();
-    let _nvs = nvs.unwrap();
-    let peripherals = peripherals.unwrap();
+    let _switchboard = switchboard.context("Startup did not provide switchboard")?;
+    let _nvs = nvs.context("Startup did not provide NVS")?;
+    let peripherals = peripherals.context("Startup did not provide peripherals")?;
 
     // led fsm
     let led_events_tx = conductor_tx.clone();
     let (led_commands_tx, led_commands_rx) = mpsc::channel();
+
     Fsm::new_async(
         Box::new(LEDHold),
         LEDContext {
@@ -56,42 +60,56 @@ fn main() -> Result<()> {
         led_events_tx,
         led_commands_rx,
         "LED Control",
-        16_384,
+        8 * 1_024,
         Duration::from_millis(100),
-    )?;
+    )
+    .context("Failed to start LED FSM")?;
 
     // maintenance fsm
     let maintenance_events_tx = conductor_tx.clone();
     let (maintenance_commands_tx, maintenance_commands_rx) = mpsc::channel();
+
     Fsm::new_async(
         Box::new(MaintenanceEnter),
         MaintenanceContext::new(peripherals.buttons),
         maintenance_events_tx,
         maintenance_commands_rx,
         "Maintenance Buttons",
-        16_384,
+        8 * 1_024,
         Duration::from_millis(100),
-    )?;
+    )
+    .context("Failed to start Maintenance FSM")?;
 
     // motion fsm
     let motion_events_tx = conductor_tx.clone();
     let (motion_commands_tx, motion_commands_rx) = mpsc::channel();
+
     Fsm::new_async(
         Box::new(MotionNotMoving),
         MotionContext::new(peripherals.motion),
         motion_events_tx,
         motion_commands_rx,
         "Motion",
-        16_384,
+        8 * 1_024,
         Duration::from_millis(100),
-    )?;
+    )
+    .context("Failed to start Motion FSM")?;
 
     loop {
-        let cmd = conductor_rx.recv().unwrap();
-        log::info!("{cmd:?}");
+        let cmd = conductor_rx.recv().context("Conductor channel closed")?;
 
-        led_commands_tx.send(cmd).unwrap();
-        maintenance_commands_tx.send(cmd).unwrap();
-        motion_commands_tx.send(cmd).unwrap();
+        info!("{cmd:?}");
+
+        if let Err(e) = led_commands_tx.send(cmd.clone()) {
+            error!("Failed to send command to LED FSM: {e}");
+        }
+
+        if let Err(e) = maintenance_commands_tx.send(cmd.clone()) {
+            error!("Failed to send command to Maintenance FSM: {e}");
+        }
+
+        if let Err(e) = motion_commands_tx.send(cmd) {
+            error!("Failed to send command to Motion FSM: {e}");
+        }
     }
 }
