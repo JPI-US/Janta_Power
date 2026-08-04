a74e11ac6f4c27a81d312fdca1379828a054ca3e
Nathan Wignall <nwignall@jantaus.com>
2026-08-04 15:27:51 -0500

    Always pull the limit switch down

--- crates/motion/src/lib.rs
+++ crates/motion/src/lib.rs
@@ -149,14 +149,7 @@ pub mod motion {
             let encoder_a = PinDriver::input(encoder_a_pin).unwrap();
             let encoder_b = PinDriver::input(encoder_b_pin).unwrap();
 
-            match limit_switch_active_level {
-                ActiveLevel::ActiveHigh => lmsw
-                    .set_pull(esp_idf_svc::hal::gpio::Pull::Down)
-                    .unwrap_or_default(),
-                ActiveLevel::ActiveLow => lmsw
-                    .set_pull(esp_idf_svc::hal::gpio::Pull::Up)
-                    .unwrap_or_default(),
-            }
+            lmsw.set_pull(esp_idf_svc::hal::gpio::Pull::Down).unwrap_or_default();
 
             let encoder = IncrementalEncoder::<Rotary, _, _, QuadStep>::new(encoder_a, encoder_b);
 
